5a60b21a6c3c08ffb2bd2b795357f64ed1b1ebd4
Ian Yap <ianyapzy@gmail.com>
2023-09-07 11:45:27 +1200

    Added tests for reported ExceptionInInitializerErrors

--- azure-android-compat/build.gradle.kts
+++ azure-android-compat/build.gradle.kts
@@ -1,3 +1,5 @@
+import com.android.build.api.dsl.Packaging
+
 plugins {
     id("com.android.application")
     //id("de.mannodermaus.android-junit5") version "1.9.3.0"
@@ -30,14 +32,25 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+
+    packagingOptions() {
+        resources.excludes.add("META-INF/*")
+    }
 }
 
 dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation(files("libs\\azure-core-1.43.0-beta.1.jar"))
     testImplementation("junit:junit:4.13.2")
+
+
+    //For running existing tests
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    //androidTestImplementation("org.junit.jupiter:junit-jupiter:5.9.1")
+    //androidTestImplementation("de.mannodermaus.junit5:android-test-core:1.3.0")
+    //androidTestRuntimeOnly("de.mannodermaus.junit5:android-test-runner:1.3.0")
 
     // For testing Alan's branch in azure-core
     // Strangely, the first two are already in the core pom.xml but still need to be called
@@ -48,26 +61,27 @@ dependencies {
     // https://mvnrepository.com/artifact/org.reactivestreams/reactive-streams
     implementation("org.reactivestreams:reactive-streams:1.0.4")
 
+    // TODO: Set up different builds to avoid constant manual dependency changes
+    // For testing issue 1540, https://github.com/microsoftgraph/msgraph-sdk-java/issues/1540
+    // https://mvnrepository.com/artifact/com.azure/azure-identity
+    implementation("com.azure:azure-identity:1.10.0")
+    //implementation(files("libs\\azure-identity-1.11.0-beta.1.jar"))
+    // https://mvnrepository.com/artifact/com.azure/azure-core
+    implementation("com.azure:azure-core:1.42.0")
+    //implementation(files("libs\\azure-core-1.43.0-beta.1.jar"))
 
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    //For testing issue 35756, https://github.com/Azure/azure-sdk-for-java/issues/35756
+    // https://mvnrepository.com/artifact/com.azure/azure-ai-translation-text
+    implementation("com.azure:azure-ai-translation-text:1.0.0-beta.1")
+    // implementation(files("libs\\azure-ai-translation-text-1.0.0-beta.2.jar"))
+    // https://mvnrepository.com/artifact/com.azure/azure-core-http-netty
+    implementation("com.azure:azure-core-http-netty:1.14.0-beta.1")
+    //implementation(files("libs\\azure-core-http-netty-1.14.0-beta.2.jar"))
+    // https://mvnrepository.com/artifact/com.fasterxml.jackson.core/jackson-databind
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.15.2")
 
-    //For running existing tests
-    //androidTestImplementation("org.junit.jupiter:junit-jupiter:5.9.1")
-    //androidTestImplementation("de.mannodermaus.junit5:android-test-core:1.3.0")
-    //androidTestRuntimeOnly("de.mannodermaus.junit5:android-test-runner:1.3.0")
+    //For testing issue 35719, https://github.com/Azure/azure-sdk-for-java/issues/35719
+    implementation("com.azure:azure-ai-openai:1.0.0-beta.2")
+    //implementation(files("libs\\azure-ai-openai-1.0.0-beta.5.jar"))
 
-    // https://mvnrepository.com/artifact/com.azure/azure-xml
-    implementation("com.azure:azure-xml:1.0.0-beta.2")
-    // https://mvnrepository.com/artifact/com.azure/azure-core-management
-    //implementation("com.azure:azure-core-management:1.11.4")
-
-    //implementation("com.azure:azure-core:1.42.0")
-    // StAX provides javax.xml.stream which Android lacks. However, it lacks the newFactory
-    // function from XMLInputFactory and XMLOutputFactory, which is called by DefaultXMLReader and
-    // DefaultXMLWriter in azure-xml. This artifact may be overly general, but more specific
-    // artifacts resulted in FactoryConfigurationErrors being thrown on newInstance(), due to
-    // com.bea.xml.stream.* being missing.
-    // https://mvnrepository.com/artifact/stax/stax
-    // implementation ("stax:stax:1.2.0")
 }
--- azure-android-compat/src/androidTest/java/com/samples/androidcompat/AzureCoreReflectionUtilsCompatInstrumentedTest.java
+++ azure-android-compat/src/androidTest/java/com/samples/androidcompat/AzureCoreReflectionUtilsCompatInstrumentedTest.java
@@ -1,10 +1,14 @@
 package com.samples.androidcompat;
 
-import static com.azure.core.implementation.ReflectionUtils.getConstructorInvoker;
+//import static com.azure.core.implementation.ReflectionUtils.getConstructorInvoker;
+//import com.azure.core.implementation.ReflectionUtils;
+import com.azure.ai.openai.OpenAIClient;
+import com.azure.ai.openai.OpenAIClientBuilder;
+import com.azure.ai.translation.text.TextTranslationClient;
+import com.azure.ai.translation.text.TextTranslationClientBuilder;
+import com.azure.core.credential.AzureKeyCredential;
 
-import android.content.Context;
-
-import androidx.test.platform.app.InstrumentationRegistry;
+import com.azure.identity.ClientSecretCredentialBuilder;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import org.junit.Test;
@@ -12,15 +16,12 @@ import org.junit.runner.RunWith;
 
 import static org.junit.Assert.*;
 
-import java.lang.invoke.MethodHandles;
-
 /**
- * Instrumented test cases for ReflectionUtils.
- * For reasons not yet certain, an API level 26 emulator seems to function correctly while
- * greater levels such as 30 and 34 throw NoSuchMethodExceptions caused by:
- * java.lang.invoke.MethodHandles$Lookup.<init>
- * Might be something to do with the Gradle build file, as the AzureAndroidValidation library does
- * not exhibit this unusual behaviour at all, its tests run as expected for all emulators
+ * Instrumented test cases for ReflectionUtils usages.
+ * When an emulator's API and an app's targetSdk are both greater than 27, ExceptionInInitializer
+ * would occur because of the constructor for MethodHandles.Lookup being missing. These three test
+ * cases replicated user-reported cases where ExceptionInInitializer would occur, and were used to
+ * test if a new PR could resolve these issues by providing two different ReflectionUtil versions.
  */
 @RunWith(AndroidJUnit4.class)
 public class AzureCoreReflectionUtilsCompatInstrumentedTest {
@@ -31,44 +32,72 @@ public class AzureCoreReflectionUtilsCompatInstrumentedTest {
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         getLookupToUse(null);
     }*/
+
+    // Tests were run with placeholder credentials but did trigger ExceptionInInitializerError.
+    /* Testing issue 1540 at https://github.com/microsoftgraph/msgraph-sdk-java/issues/1540.
+     * Results for current azure-core version (1.42.0):
+     * - API level = 26, 27: IllegalArgumentException (invalid TENANT_ID)
+     * - API level > 27: ExceptionInInitializerError (MethodHandles.Lookup)
+     *
+     * Results for PR version:
+     * - API level >= 26: IllegalArgumentException (invalid TENANT_ID)
+     * This indicates that the PR has managed to resolve the ExceptionInInitializerError.
+     */
     @Test
-    // Test Alan's reworked ReflectionUtils that should be more compatible with Android
-    public void coreReflectionUtilsTest() throws Exception {
+    public void identityTest() {
         // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        //Throws NullPointerException in API level 26, returns Invoker with higher API levels
-        assertThrows(NullPointerException.class, () -> {
-            getConstructorInvoker(null, null);
-        });
-    }
-    @Test
-    public void coreReflectionUtilsWithClassTest() throws Exception {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        //Throws IllegalArgumentException in API level 26, returns Invoker at higher API levels
+        String TENANT_ID = "<redacted>";
+        String CLIENT_ID = "<redacted>";
+        String CLIENT_SECRET = "<redacted>";
+
         assertThrows(IllegalArgumentException.class, () -> {
-            getConstructorInvoker(Class.class, null);
+            new ClientSecretCredentialBuilder().clientId(CLIENT_ID).tenantId(TENANT_ID)
+                    .clientSecret(CLIENT_SECRET).build();
         });
     }
 
+    /* Testing issue 35756, https://github.com/Azure/azure-sdk-for-java/issues/35756
+     * Results for current azure-core version (1.42.0):
+     * - API level = 26, 27: no errors thrown
+     * - API level > 27: ExceptionInInitializerError thrown
+     *
+     * Results for PR version:
+     * - API >= 26: NoClassDefFoundError, missing dependencies that are already in the pom.xml.
+     * This NoClassDefFoundError occurs about 30 lines after the ExceptionInInitializerError in
+     * the createHttpPipeline() method of TextTranslationClientBuilder, so that indicates the PR has
+     * resolved the issue.
+     */
     @Test
-    public void coreReflectionUtilsWithClassConstructor() throws Exception {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        // Returns NoSuchMethodException for all emulators
-        assertThrows(NoSuchMethodException.class, () -> {
-            getConstructorInvoker(Class.class,
-                    Class.class.getDeclaredConstructor(Class.class));
-        });
+    public void aiTranslationTest() {
+        String credential = "<redacted>";
+        String region = "<redacted>";
+        TextTranslationClient client = new TextTranslationClientBuilder()
+                .credential(new AzureKeyCredential(credential))
+                .region(region)
+                .buildClient();
+        assertNotNull(client);
     }
+
+    /* Testing issue 35719, https://github.com/Azure/azure-sdk-for-java/issues/35719
+     * Results for current azure-core version (1.42.0):
+     * Emulator and targetSdk API level = 26 or 27: no errors thrown
+     * Emulator and targetSdk API level > 27: ExceptionInInitializerError thrown.
+     *
+     * Results for PR version:
+     * NoClassDefFoundError for API >= 26, missing dependencies like Netty that are in the pom.xml.
+     * Tracing the logs, this error appears to occur further along than the
+     * ExceptionInInitializerError in the buildClient() of OpenAIClientBuilder, which seems to
+     * indicate that it's resolved the issue.
+     */
     @Test
-    public void coreReflectionUtilsWithMethodHandleConstructor() throws Exception {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        // Returns IllegalArgumentException for 26, and NoSuchMethodException for higher levels
-        assertThrows(IllegalArgumentException.class, () -> {
-            getConstructorInvoker(MethodHandles.Lookup.class,
-                    MethodHandles.Lookup.class.getDeclaredConstructor(Class.class));
-        });
+    public void aiTest() {
+        String AZURE_OPEN_AI_KEY = "<redacted>";
+        String AZURE_OPEN_AI_ENDPOINT = "<redacted>";
+
+        OpenAIClient client = new OpenAIClientBuilder()
+                .credential(new AzureKeyCredential(AZURE_OPEN_AI_KEY))
+                .endpoint(AZURE_OPEN_AI_ENDPOINT)
+                .buildClient();
+        assertNotNull(client);
     }
 }
