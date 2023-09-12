6a16d2d4beb74cb2ca0e418d0c3bbc37b001907d
Saya <sh5155@gmail.com>
2023-09-13 11:13:01 +1200

    Comment out whole Xml test file
    Seems like it just doesn't want to run if there are no tests inside

--- azure-android-compat/src/androidTest/java/com/samples/androidcompat/AzXmlInstrumentedTests.java
+++ azure-android-compat/src/androidTest/java/com/samples/androidcompat/AzXmlInstrumentedTests.java
@@ -1,55 +1,55 @@
-package com.samples.androidcompat;
-
-import static org.junit.Assert.assertNotNull;
-
-import android.content.Context;
-
-import androidx.test.ext.junit.runners.AndroidJUnit4;
-import androidx.test.platform.app.InstrumentationRegistry;
-
-import com.azure.xml.XmlReader;
-import com.azure.xml.XmlWriter;
-import com.azure.xml.implementation.DefaultXmlReader;
-import com.azure.xml.implementation.DefaultXmlWriter;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-
-import java.io.ByteArrayOutputStream;
-
-import javax.xml.stream.XMLStreamException;
-
-/**
- * Instrumented tests, which will execute on an Android device.
- * These test cases are for cases where the Android user has already installed the necessary
- * dependencies to run the Azure SDK, so it validates that the dependencies contain all of the
- * necessary libraries and methods.
- */
-@RunWith(AndroidJUnit4.class)
-public class AzXmlInstrumentedTests {
-    // TODO: Update comments
-    /**
-     * Tests whether DefaultXMLReader can make an XmlReader. Will throw NoSuchMethodError because
-     * the StAX library is missing XmlInputFactory.newFactory() which is in the JDK, it only has
-     * the XmlInputFactory.newInstance() method which should function identically to newFactory()
-     */
-//    @Test
-//    public void defaultXmlReaderTest() {
-//        // Context of the app under test.
-//        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-//        try {
-//            XmlReader reader = DefaultXmlReader.fromString("");
-//            assertNotNull(reader);
-//        } catch (XMLStreamException e) {
-//            throw new RuntimeException(e);
-//        }
-//    }
-
-    /**
-     * Tests whether DefaultXMLWriter can make an XmlWriter. Will throw NoSuchMethodError because
-     * the StAX library is missing XmlOutputFactory.newFactory() which is in the JDK, it only has
-     * the XmlOutputFactory.newInstance() method which should function identically to newFactory()
-     */
+//package com.samples.androidcompat;
+//
+//import static org.junit.Assert.assertNotNull;
+//
+//import android.content.Context;
+//
+//import androidx.test.ext.junit.runners.AndroidJUnit4;
+//import androidx.test.platform.app.InstrumentationRegistry;
+//
+//import com.azure.xml.XmlReader;
+//import com.azure.xml.XmlWriter;
+//import com.azure.xml.implementation.DefaultXmlReader;
+//import com.azure.xml.implementation.DefaultXmlWriter;
+//
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//
+//import java.io.ByteArrayOutputStream;
+//
+//import javax.xml.stream.XMLStreamException;
+//
+///**
+// * Instrumented tests, which will execute on an Android device.
+// * These test cases are for cases where the Android user has already installed the necessary
+// * dependencies to run the Azure SDK, so it validates that the dependencies contain all of the
+// * necessary libraries and methods.
+// */
+//@RunWith(AndroidJUnit4.class)
+//public class AzXmlInstrumentedTests {
+//    // TODO: Update comments
+//    /**
+//     * Tests whether DefaultXMLReader can make an XmlReader. Will throw NoSuchMethodError because
+//     * the StAX library is missing XmlInputFactory.newFactory() which is in the JDK, it only has
+//     * the XmlInputFactory.newInstance() method which should function identically to newFactory()
+//     */
+////    @Test
+////    public void defaultXmlReaderTest() {
+////        // Context of the app under test.
+////        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+////        try {
+////            XmlReader reader = DefaultXmlReader.fromString("");
+////            assertNotNull(reader);
+////        } catch (XMLStreamException e) {
+////            throw new RuntimeException(e);
+////        }
+////    }
+//
+//    /**
+//     * Tests whether DefaultXMLWriter can make an XmlWriter. Will throw NoSuchMethodError because
+//     * the StAX library is missing XmlOutputFactory.newFactory() which is in the JDK, it only has
+//     * the XmlOutputFactory.newInstance() method which should function identically to newFactory()
+//     */
 //    @Test
 //    public void defaultXmlWriterTest() {
 //        // Context of the app under test.
@@ -62,4 +62,4 @@ public class AzXmlInstrumentedTests {
 //            throw new RuntimeException(e);
 //        }
 //    }
-}
+//}
